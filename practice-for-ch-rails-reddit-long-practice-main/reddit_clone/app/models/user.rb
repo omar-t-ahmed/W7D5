0e605ecbc65e38db5e70136d96095c1537c71eac
omar-t-ahmed <omartahmed@gmail.com>
2023-10-06 09:48:37 -0400

    add user model methods

--- practice-for-ch-rails-reddit-long-practice-main/reddit_clone/app/models/user.rb
+++ practice-for-ch-rails-reddit-long-practice-main/reddit_clone/app/models/user.rb
@@ -1,2 +1,46 @@
 class User < ApplicationRecord
+    before_validation :ensure_session_token
+
+    attr_reader :password
+
+    validates :username, :session_token, :password_digest, presence: true
+    validates :username, :session_token, uniqueness: true
+    validates :password, length: {minimum: 6, allow_nil: true}
+
+    def self.find_by_credentials(username, password)
+        @user = User.find_by(username: username)
+
+        if @user && @user.is_password?(password)
+            @user
+        else 
+            nil
+        end
+    end
+
+    def password=(password)
+        @password = password
+        self.password_digest = Bcrypt::Password.create(password)
+    end
+
+    def is_password?(password)
+        password_object = BCrypt::Password.new(password_digest)
+        password_object.is_password?(password)
+    end
+
+    def reset_session_token!
+        self.session_token = generate_session_token
+        self.save!
+        self.session_token
+    end
+
+    private
+
+    def generate_session_token
+        SecureRandom::urlsafe_base64
+    end
+
+    def ensure_session_token
+        self.session_token ||= generate_session_token
+    end
+
 end
