b917c73a9cfca5c3ce828b0d467a3bbe3da69c9f
omar-t-ahmed <omartahmed@gmail.com>
2023-10-06 10:10:20 -0400

    users controller

--- practice-for-ch-rails-reddit-long-practice-main/reddit_clone/app/controllers/users_controller.rb
+++ practice-for-ch-rails-reddit-long-practice-main/reddit_clone/app/controllers/users_controller.rb
@@ -1,2 +1,37 @@
 class UsersController < ApplicationController
+
+    def new
+        @user = User.new
+        render :new
+    end
+
+    def index
+        @users = User.all
+        render :index
+    end
+
+    def show
+        @user = User.find_by(id: params[:id])
+        render :show
+    end
+
+    def create
+        @user = User.new(user_params)
+
+        if @user.save
+            login(@user)
+            redirect_to users_url
+        else
+            flash.now[:errors] = @user.errors.full_messages
+            @user = User.new(username: params[:username])
+            render :new
+        end
+    end
+
+    private
+
+    def user_params
+        params.require(:user).permit(:username, :password)
+    end
+
 end
