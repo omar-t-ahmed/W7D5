0e2cb9898c7c8257c8270cfb212f7a6c91d1a59f
“Eltion <ebehrami01@gmail.com>
2023-10-06 10:22:19 -0400

    sessions controller built

--- practice-for-ch-rails-reddit-long-practice-main/reddit_clone/app/controllers/sessions_controller.rb
+++ practice-for-ch-rails-reddit-long-practice-main/reddit_clone/app/controllers/sessions_controller.rb
@@ -1,2 +1,29 @@
 class SessionsController < ApplicationController
+    before_action :require_logged_in, only: [:destroy] 
+    before_action :require_logged_out, only: [:new, :create]
+
+
+    def new 
+        @user = User.new 
+        render :new
+    end 
+
+    def create 
+        @user = User.find_by_credentials(params[:user][:username], params[:user][:password ])
+        if @user 
+            login!(@user)
+            redirect_to user_url(@user)
+        else 
+            @user = User.new(username: params[:user][:username])
+            flash.now[:errors] = ["Invalid username or password"]
+            render :new 
+        end 
+    end 
+
+    def destroy
+        logout!
+        redirect_to new_session_url 
+    end 
+
+
 end
--- practice-for-ch-rails-reddit-long-practice-main/reddit_clone/app/controllers/users_controller.rb
+++ practice-for-ch-rails-reddit-long-practice-main/reddit_clone/app/controllers/users_controller.rb
@@ -1,5 +1,7 @@
 class UsersController < ApplicationController
-
+    before_action :require_logged_in, only: [:index, :show]
+    before_action :require_logged_out, only: [:new, :create]
+    
     def new
         @user = User.new
         render :new
@@ -19,7 +21,7 @@ class UsersController < ApplicationController
         @user = User.new(user_params)
 
         if @user.save
-            login(@user)
+            login!(@user)
             redirect_to users_url
         else
             flash.now[:errors] = @user.errors.full_messages
